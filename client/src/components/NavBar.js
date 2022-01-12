7b9ab544ff276baa2b2a9ba3c09f68bb25bc02c2
Dustin2018 <dd_jennings@hotmail.com>
2022-01-11 19:04:43 -0700

    useless update

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./NavBar.css";
+import { Link, useNavigate } from "react-router-dom";
+
+const NavBar = () => {
+  const navigate = useNavigate();
+
+  const navigateHome = () => {
+    navigate("/");
+  };
+
+  return (
+    <div>
+      <span>
+        <h1 onClick={navigateHome}>Rabbit Hole</h1>
+      </span>
+    </div>
+  );
+};
+
+export default NavBar;
