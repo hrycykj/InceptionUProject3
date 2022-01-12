e8bb7fe7252b636faa08a59013ec1bc00322a1ec
Dustin2018 <dd_jennings@hotmail.com>
2022-01-11 19:07:22 -0700

    another useless update

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./NavBar.css";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
   const navigate = useNavigate();
