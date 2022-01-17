993bce62da6f9c773a6d4d4630ba19d1a3e99460
Dustin2018 <dd_jennings@hotmail.com>
2022-01-17 07:30:20 -0700

    main page area styling

--- client/src/components/MainArea.js
+++ client/src/components/MainArea.js
@@ -1,11 +1,16 @@
 import React from 'react'
-import 'MainArea.css'
+import './MainArea.css'
 
 const MainArea = () => {
     return (
         <div>
+    <div class="content">
+            <div class="text">
             <h1>Start Your Adventure</h1>
-        </div>
+            </div>
+            
+           </div>
+           </div>
     )
 
 }
