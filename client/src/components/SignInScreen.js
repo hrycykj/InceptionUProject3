c73150b033cc753b91dce9f732b5ca7d34d116df
Your Name <hannahlo_26@hotmail.com>
2022-01-17 23:42:10 -0700

    "fixed" dependency errors

--- client/src/components/SignInScreen.js
+++ client/src/components/SignInScreen.js
@@ -20,7 +20,7 @@ firebase.initializeApp(config);
 const uiConfig = {
   // Popup signin flow rather than redirect flow.
   signInFlow: 'popup',
-  // We will display Google and Facebook as auth providers.
+  // Email, Google and Facebook as auth providers. Apple required payment.
   signInOptions: [
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     firebase.auth.FacebookAuthProvider.PROVIDER_ID, 
