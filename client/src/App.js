bc93bb76b76624cbc6e19be3b5504c6ad4717544
hrycykj <john@quillconsulting.ca>
2022-01-17 17:53:12 -0700

    commented out the react-native splash screen

--- client/src/App.js
+++ client/src/App.js
@@ -20,13 +20,13 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <Button variant="contained" onClick={() => pingServer()} >Text</Button>
-      </header>  
+      </header>   */}
       {/* <Login />  */}
       <NavBar />
       <SignInScreen />
