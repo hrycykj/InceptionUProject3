25bfbf7e0dd1ee8e1e2393a73d9641ba23d522ee
Woojae <kwj280@gmail.com>
2022-01-21 02:55:41 -0700

    add localtunnel server

--- client-mobile/App.js
+++ client-mobile/App.js
@@ -2,16 +2,17 @@
 import React from "react";
 import { Text, View } from "react-native";
 import Login from "./components/Login";
-
+import {HOST_SERVER} from './util/hostServer'
 
 export default function App() {
+  console.log(HOST_SERVER)
 
+  fetch(`${HOST_SERVER}/api/checkPoint`).then(res=>res.json()).then(data=>{
+    console.log(data)
+  })
   return (
     <View>
           <Login />
     </View>
   );
 }
-
-
-//hello Woojea :D
