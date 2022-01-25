70f593e0454678736ba08f8c0f8a8b44ad43a250
Dustin2018 <dd_jennings@hotmail.com>
2022-01-25 11:28:46 -0700

    routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,39 @@
-
-import './App.css';
-import Button from '@mui/material/Button'
-import SignInScreen from './components/SignInScreen'
-import MainArea from './components/MainArea';
-import NavBar from './components/NavBar';
-import SideNavBar from './components/SideNavBar';
-import Footer from './components/Footer';
-import axios from 'axios'
-import { Route } from "react-router-dom";
-
-
-
+import "./App.css";
+import Button from "@mui/material/Button";
+import SignInScreen from "./components/SignInScreen";
+import MainArea from "./components/MainArea";
+import NavBar from "./components/NavBar";
+import SideNavBar from "./components/SideNavBar";
+import Footer from "./components/Footer";
+import axios from "axios";
+import { Routes, Route } from "react-router-dom";
+import About from "./pages/About";
 
 function App() {
-
   const pingServer = async () => {
-    let res = await fetch('/api/ping')
-    let json = await res.json()
-    console.log(json)
-  }
+    let res = await fetch("/api/ping");
+    let json = await res.json();
+    console.log(json);
+  };
 
   return (
-    
     <div className="App">
       <NavBar />
       {/* <SignInScreen /> */}
       {/* <SideNavBar /> */}
-      <MainArea />
-      <Footer />
-      
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <MainArea /> <Footer />{" "}
+            </div>
+          }
+        ></Route>
+        <Route path="/about" element={<div><About /> <Footer /> </div>}></Route>
+      </Routes>
     </div>
-    
   );
 }
 
