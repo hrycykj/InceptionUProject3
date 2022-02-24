76ec47d8e55de0d85ec403f7d6f3a4d433c18e40
Tleitch <tristen.leitch@gmail.com>
2022-02-24 16:08:39 -0700

    Added Flourish and Udate coin function

--- client-mobile/components/QuestFlourishFAB.js
+++ client-mobile/components/QuestFlourishFAB.js
@@ -1,18 +1,44 @@
 import React, { useState, useEffect, useContext } from "react";
-import { View, StyleSheet, Image, Dimensions } from "react-native";
+import { View, StyleSheet, Image, Dimensions, Alert } from "react-native";
 import { Text, Button, FAB, useTheme } from 'react-native-paper'
 import { Icon } from 'react-native-elements';
+import { QuestContext } from "../context/QuestContext"
 
 
 
-const QuestFlourish = () => (
-    <FAB
+
+const QuestFlourish = () => {
+  const questContext = useContext(QuestContext)
+  const checkPointIndex = questContext.checkPointIndex
+  const questDescriptor = questContext.quest.questFlourish
+  console.log("boom quest", questContext.quest)
+  return(
+    <>
+    <View>
+      {(checkPointIndex == 0) &&
+      <FAB
       style={styles.fab}
       
       icon="exclamation"
-      onPress={() => console.log('Pressed')}
-    />
-  );
+      onPress={() => {
+        Alert.alert(
+          "Quest Details",
+          questDescriptor,
+          [
+            {
+              text: "Back to Map",
+              onPress: () => {
+
+              }
+            },
+            
+          ]
+          ); 
+        }}
+      />}
+    </View>
+    </>
+  )}
   
   const styles = StyleSheet.create({
     fab: {
